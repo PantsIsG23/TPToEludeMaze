5b6af3222c51d52e555402615c5b75167ddd261d
TheExploitinGameRoom <110318476+PantsIsG23@users.noreply.github.com>
2025-01-20 13:35:18 +0800

    Create BARZIL.lua

---
+++ BARZIL.lua
@@ -0,0 +1,15 @@
+-- Once again had made another script for Barzil, now you can get to the portal with no problem!!!
+
+
+ local TeleportService = game:GetService("TeleportService")
+
+local Place = 7234087065
+
+local player = game.Players.LocalPlayer
+
+TeleportService:Teleport(Place, player)
+
+
+-- Credits to: Pastebin user: "IMBACKWITHTHEMILK" so that I don't skid this.
+-- Also.. Please don't skid this project.. Or else..
+-- THIS WILL TP YOU INSTANTLY! SO BE AWARE!
